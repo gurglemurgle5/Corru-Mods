bf9036e2def489194486edcaaf0e3b72f56859b5
Gurglemurgle5 <95941332+gurglemurgle5@users.noreply.github.com>
2025-04-24 21:49:42 -0500

    update urls

--- pwa.js
+++ pwa.js
@@ -3,6 +3,6 @@
 // TODO: not a priority but idk if this will persist when going to different pages, might wanna set it up if necessary
 let node = document.createElement("link");
 node.rel = "manifest";
-node.href = "http://localhost:8080/manifest.json";
+node.href = "https://corru.gurglemurgle.dev/manifest.json";
 
 document.head.appendChild(node);
