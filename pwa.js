03e98e57792754967222c47b44560fce005f35af
Gurglemurgle5 <95941332+gurglemurgle5@users.noreply.github.com>
2025-10-22 21:10:36 -0500

    update pwa.js to work across corru subdomains
    also corruskivi because why not
    deleting the gifs because chromium doesn't use them anyways

--- pwa.js
+++ pwa.js
@@ -1,8 +1,48 @@
 "use strict";
 
-// TODO: not a priority but idk if this will persist when going to different pages, might wanna set it up if necessary
+const manifest = {
+  name: "corru.observer",
+  icons: [
+    {
+      src: "https://corru.gurglemurgle.dev/fav192.png",
+      type: "image/png",
+      sizes: "192x192"
+    },
+    {
+      src: "https://corru.gurglemurgle.dev/fav512.png",
+      type: "image/png",
+      sizes: "512x512"
+    }
+  ],
+  "start_url": "https://corru.observer",
+  "display": "fullscreen",
+  "display_override": [
+    "fullscreen",
+    "standalone",
+    "minimal-ui",
+    "browser"
+  ],
+  "screenshots": [
+    {
+      "src": "https://corru.observer/img/socials/intro.gif",
+      "sizes": "600x400",
+      "type": "image/gif",
+      "form_factor": "wide"
+    }
+  ]
+};
+
+let start_url = window.location.origin;
+// idk if there's a good way to automatically detect the home page, so just i'm just hardcoding it for this specific case
+if (start_url == "https://crxb.cc") {
+  start_url = "https://crxb.cc/corruskivi/";
+  manifest.name = "corruskivi"
+}
+
+manifest.start_url = start_url;
+
 let node = document.createElement("link");
 node.rel = "manifest";
-node.href = "https://corru.gurglemurgle.dev/manifest.json";
+node.href = "data:application/json," + encodeURIComponent(JSON.stringify(manifest));
 
 document.head.appendChild(node);
